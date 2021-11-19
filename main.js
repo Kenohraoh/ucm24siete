e561d39c2bf57c725b54b1c4a1087329ff99cf92
Kenohraoh <94694534+Kenohraoh@users.noreply.github.com>
2021-11-19 12:39:03 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -6,3 +6,34 @@ document.querySelector(".menu-btn").addEventListener("click", () => {
   ScrollReveal().reveal('.news-cards', { delay: 500 });
   ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
   ScrollReveal().reveal('.social', { delay: 500 });
+
+  popupWhatsApp = () => {
+  
+    let btnClosePopup = document.querySelector('.closePopup');
+    let btnOpenPopup = document.querySelector('.whatsapp-button');
+    let popup = document.querySelector('.popup-whatsapp');
+    let sendBtn = document.getElementById('send-btn');
+  
+    btnClosePopup.addEventListener("click",  () => {
+      popup.classList.toggle('is-active-whatsapp-popup')
+    })
+    
+    btnOpenPopup.addEventListener("click",  () => {
+      popup.classList.toggle('is-active-whatsapp-popup')
+       popup.style.animation = "fadeIn .6s 0.0s both";
+    })
+    
+    sendBtn.addEventListener("click", () => {
+    let msg = document.getElementById('whats-in').value;
+    let relmsg = msg.replace(/ /g,"%20");
+       
+     window.open('https://wa.me/+56965645390?text='+relmsg, '_blank'); 
+    
+    });
+  
+    setTimeout(() => {
+      popup.classList.toggle('is-active-whatsapp-popup');
+    }, 3000);
+  }
+  
+  popupWhatsApp();
